50b638f9fce773fe46c8f221ed5fca34ee1c90bc
saku <saku112358@gmail.com>
2025-10-05 19:19:43 +0900

    search page init

---
+++ frontend/src/app/search/page.tsx
@@ -0,0 +1,17 @@
+// src/app/page.tsx
+
+import Link from 'next/link';
+import { FaChevronRight } from 'react-icons/fa'; 
+
+export default function HomePage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold text-gray-800 mb-2">
+        検索
+      </h1>
+      <p className="text-gray-600 mb-8">
+      </p>
+
+    </main>
+  );
+}
